54ac3485cc4afbc595a99b16a8f4dca95739a358
Sujuy <63218621+Ssujuy@users.noreply.github.com>
2023-06-02 09:25:05 +0300

    Add files via upload

--- skyscr.pl
+++ skyscr.pl
@@ -6,13 +6,18 @@ skyscr(Id, Solution) :-
        puzzle(Id, Max, Left, Right, Top, Bottom , Board),
        Solution = Board,
        Solution#:: 1..Max,
+       different_list(Solution),
+       different_list_y(Solution,1),
        left_skyscr(Left, Solution),
-       search(Solution,0,input_order,indomain,complete,[]).
+       right_skyscr(Right, Solution),
+       top_skyscr(Top,1,Solution),
+       bottom_skyscr(Bottom,1,Solution),
+       search(Solution,0,most_constrained ,indomain,complete,[]),!.
 
 
 left_skyscr(Left, Solution) :-
        [Hl | Tl] = Left,
-       [Hs | Ts] = Solution,
+       [_ | Ts] = Solution,
        Hl = 0,
        left_skyscr(Tl, Ts).
 
@@ -23,37 +28,105 @@ left_skyscr(Left, Solution) :-
        length(Hs, Len),
        length(Visible, Len),
        Visible#:: 0..1,
-       constrain_left_skyscr(Hs,Visible,0),
+       constrain_skyscr(Hs,Visible,0),
        find_sum(Visible,0,TotalSum),
        TotalSum#=Hl, 
        left_skyscr(Tl, Ts).
 
 left_skyscr([],[]).
 
+right_skyscr(Right, Solution) :-
+       [Hr | Tr] = Right,
+       [_ | Ts] = Solution,
+       Hr = 0,
+       right_skyscr(Tr, Ts).
 
-constrain_left_skyscr(L,Visible,0) :-
+right_skyscr(Right, Solution) :-
+       [Hr | Tr] = Right,
+       [Hs | Ts] = Solution,
+       Hr \= 0,
+       length(Hs, Len),
+       length(Visible, Len),
+       Visible#:: 0..1,
+       reverse_list(Hs,[],HsReverse),
+       constrain_skyscr(HsReverse,Visible,0),
+       find_sum(Visible,0,TotalSum),
+       TotalSum#=Hr, 
+       right_skyscr(Tr, Ts).  
+
+right_skyscr([],[]).
+
+top_skyscr(Top, Counter, Solution) :-
+       [Ht | Tt] = Top,
+       Ht = 0,
+       NCounter is Counter + 1,
+       top_skyscr(Tt,NCounter,Solution).
+
+top_skyscr(Top, Counter,Solution) :-
+       [Ht | Tt] = Top,
+       Ht \= 0,
+       length(Solution, Len),
+       length(Visible, Len),
+       Visible#:: 0..1,
+       find_list(Solution,Counter,1,Len,YList),
+       constrain_skyscr(YList,Visible,0),
+       find_sum(Visible,0,TotalSum),
+       TotalSum#=Ht, 
+       NCounter is Counter + 1,
+       top_skyscr(Tt,NCounter,Solution).
+
+top_skyscr([],Counter,Solution) :-
+       length(Solution,Len),
+       Len < Counter.
+
+bottom_skyscr(Bottom, Counter, Solution) :-
+       [Hb | Tb] = Bottom,
+       Hb = 0,
+       NCounter is Counter + 1,
+       bottom_skyscr(Tb,NCounter,Solution).
+
+bottom_skyscr(Bottom, Counter,Solution) :-
+       [Hb | Tb] = Bottom,
+       Hb \= 0,
+       length(Solution, Len),
+       length(Visible, Len),
+       Visible#:: 0..1,
+       find_list(Solution,Counter,1,Len,YList),
+       reverse_list(YList,[],ReversedYList),
+       constrain_skyscr(ReversedYList,Visible,0),
+       find_sum(Visible,0,TotalSum),
+       TotalSum#=Hb, 
+       NCounter is Counter + 1,
+       bottom_skyscr(Tb,NCounter,Solution).
+
+bottom_skyscr([],Counter,Solution) :-
+       length(Solution,Len),
+       Len < Counter.
+
+
+constrain_skyscr(L,Visible,0) :-
        [Hv | Tv] = Visible,
        [H | T] = L,
        Hv#=1,
-       constrain_left_skyscr(T,Tv,H).
+       constrain_skyscr(T,Tv,H).
 
-constrain_left_skyscr(L,Visible,MaxVar) :-
+constrain_skyscr(L,Visible,MaxVar) :-
        MaxVar \= 0,
        [Hv | Tv] = Visible,
        [H | T] = L,
        H #> MaxVar,
        Hv#=1,
-       constrain_left_skyscr(T,Tv,H).
+       constrain_skyscr(T,Tv,H).
 
-constrain_left_skyscr(L,Visible,MaxVar) :-
+constrain_skyscr(L,Visible,MaxVar) :-
        MaxVar \= 0,
        [Hv | Tv] = Visible,
        [H | T] = L,
        H #< MaxVar,
        Hv#=0,
-       constrain_left_skyscr(T,Tv,MaxVar).
+       constrain_skyscr(T,Tv,MaxVar).
 
-constrain_left_skyscr([],[],_).
+constrain_skyscr([],[],_).
 
 
 find_sum(Visible,Sum,TotalSum) :-
@@ -63,8 +136,69 @@ find_sum(Visible,Sum,TotalSum) :-
 
 find_sum([],TotalSum,TotalSum).
 
+find_list(Solution,X,Y,Length,YList) :-
+       YList = [H | T],
+       index_xy(Solution,X,Y,1,1,Target),
+       H = Target,
+       NY is Y + 1,
+       find_list(Solution,X,NY,Length,T).
 
+find_list(_,_,NY,Length,[]) :-  NY > Length.
 
+different_list_y(Solution,Counter) :-
+       length(Solution,Len),
+       Counter =< Len,
+       find_list(Solution,Counter,1,Len,YList),
+       unique_element(YList),
+       NCounter is Counter + 1,
+       different_list_y(Solution,NCounter).
+
+different_list_y(Solution,Counter) :-
+       length(Solution,Len),
+       Counter > Len.
+
+different_list(Solution) :-
+       [H | T] = Solution,
+       unique_element(H),
+       different_list(T).
+
+different_list([]).
+
+smaller_element(X,L) :-
+    L \= [],
+    [H | T] = L,
+    X #\= H,
+    smaller_element(X,T).
+
+smaller_element(_,[]).
+
+unique_element(L) :-
+    L \= [],
+    [H | T] = L,
+    smaller_element(H,T),
+    unique_element(T).
+
+unique_element([]).
+
+index_xy(L,X,Y,XCounter,YCounter,Target) :-
+       index_y(L,Y,YCounter,YL),
+       index_x(YL,X,XCounter,Target).
+
+index_x(L,X,XCounter,Target) :-
+       XCounter < X,
+       [_ | T] = L,
+       NXCounter is XCounter + 1,
+       index_x(T,X,NXCounter,Target).
+
+index_x([H | _],Y,Y,H).
+
+index_y(L,Y,YCounter,Target) :-
+       YCounter < Y,
+       [_ | T] = L,
+       NYCounter is YCounter + 1,
+       index_y(T,Y,NYCounter,Target).
+
+index_y([H | _],Y,Y,H).
 
 set_length(L,N) :-
        [H | T] = L,
@@ -73,6 +207,11 @@ set_length(L,N) :-
 
 set_length([],_).
 
+reverse_list([],Reversed,Reversed).
+
+reverse_list([H|T],T1,Reversed) :- reverse_list(T,[H|T1],Reversed).
+
+reverse_list([],Z,Z).
 
 puzzle(demo, 5,
        [0,2,0,2,4], [4,0,2,0,0],
